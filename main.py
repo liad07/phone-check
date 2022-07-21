c7de7849c55918edcccc4949adbe76a08e1c4c4d
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-21 22:56:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import geocoder,carrier
-x = phonenumbers.parse("+972509507172", None)
+x = phonenumbers.parse(input("enter phone number\n"), None)
 print(x)
 print("valid number:"+f"{phonenumbers.is_valid_number(x)}")
 print("possibility number:"+f"{phonenumbers.is_possible_number(x)}")
